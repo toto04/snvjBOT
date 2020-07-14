3290728e7b7e7a9e820024a47e266fc04f2cd023
toto04 <tommaso.morganti01@gmail.com>
2020-07-14 03:36:12 +0200

    clean the song requests!

--- index.ts
+++ index.ts
@@ -1,66 +1,16 @@
-import discord from 'discord.js'
-import commands from './commands'
-import Player from './player'
-import http from 'http'
-import fs from 'fs'
-import env from 'dotenv'
-env.config()
+import { config } from 'dotenv'
+config()
+import Discord from 'discord.js'
 
-const client = new discord.Client();
+const client = new Discord.Client();
 
-var summonChar = "!"
-var player: Player
-
-client.on("ready", () => {
-  fs.exists('audio_cache', e => {
-    if (!e) {
-      console.log('creating audio cache')
-      fs.mkdirSync('audio_cache')
-      fs.writeFileSync('audio_cache/sizes.json', '[]')
-    }
-  })
-
-  console.log("Snavajab bot up to work!");
-  console.log(client.user.username);
-
-  player = new Player(client);
-
-  client.user.setPresence({
-    game: { name: "music bot", type: 'PLAYING' },
-    status: 'online',
-    afk: true
-  });
+client.on('ready', async () => {
+    console.log('snvjBOT ready to work!')
 })
 
-client.on("message", (message) => {
-  if (message.content[0] != summonChar) return; //return if it's not a command
-  if (!message.guild) return; //return if it's a bot
-
-  let spcIdx = message.content.indexOf(' ');
-  let command: string 
-  let payload: string | undefined
-
-  if (spcIdx >= 0) {
-    command = message.content.substr(1, message.content.indexOf(' ') - 1);
-    payload = message.content.substr(message.content.indexOf(' ') + 1, message.content.length);
-  } else {
-    command = message.content.substr(1, message.content.length);
-  }
-
-  if (commands[command]) commands[command].exec(message, player, payload)
-  else (message.reply(`Unknows command, type ${summonChar}help for a list of available`))
+client.on('message', async message => {
+    if (message.channel.id != '718834639392997497') return
+    setTimeout(() => message.delete(), 15 * 60 * 1000)
 })
 
-//QUESTO È IL MIO TOKEN E DI NESSUN ALTRO. No sul serio, questo token serve al login con il bot
-client.login(process.env.TOKEN);
-
-const port = process.env.PORT || 5000
-
-http.createServer((req, res) => {
-  console.log('lmao')
-  res.end('not implemented')
-}).listen(port)
-
-setInterval(() => {
-  http.get(`http://snvjbot.herokuapp.com`)
-}, 300000)
+client.login(process.env.TOKEN)
